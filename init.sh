2dc5c7e7ad1fd15dbcb4683949364ad5daad3da6
Ваше Имя <you@example.com>
2021-07-11 22:05:36 +0300

    Configure repo. Install openvpn

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+## Configure repo openvpn. See https://community.openvpn.net/openvpn/wiki/OpenvpnSoftwareRepos
+##
+apt update && apt -y install ca-certificates wget curl net-tools gnupg lsb-release
+wget -qO- https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
+echo "deb http://build.openvpn.net/debian/openvpn/stable $(lsb_release -cs) main" > /etc/apt/sources.list.d/openvpn.list
+
+## Install Openvpn
+apt update && apt install openvpn
